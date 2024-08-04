0a0afcce176556ddbeb7dc4f916bdedb25d52026
Julio <juliobcp@gmail.com>
2024-08-04 15:39:01 -0300

    Adicionando @Autowired no cartaoCreditoService do controller

--- mscartaocredito/src/main/java/br/com/fiap/controller/CartaoCreditoController.java
+++ mscartaocredito/src/main/java/br/com/fiap/controller/CartaoCreditoController.java
@@ -5,6 +5,7 @@ import br.com.fiap.infra.exception.RegraNegocioException;
 import br.com.fiap.model.CartaoCredito;
 import br.com.fiap.model.CartaoCreditoDTO;
 import br.com.fiap.service.CartaoCreditoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authorization.AuthorizationDeniedException;
@@ -22,6 +23,7 @@ import java.util.NoSuchElementException;
 @RequestMapping("/api/cartao")
 public class CartaoCreditoController {
 
+    @Autowired
     CartaoCreditoService cartaoCreditoService;
 
     public CartaoCreditoController() {
