be4c6b35859a5a121fa3ffa8c8a9896b2dac8006
Thiago Vargas <thiagorv16957@gmail.com>
2024-08-07 18:02:34 -0300

    testes

--- mspagamento/src/test/java/br/com/fiap/mspagamento/config/ConfigTest.java
+++ mspagamento/src/test/java/br/com/fiap/mspagamento/config/ConfigTest.java
@@ -1,4 +1,33 @@
 package br.com.fiap.mspagamento.config;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
 public class ConfigTest {
+    // ObjectMapper bean is created successfully
+    @Test
+    public void test_object_mapper_creation_success() {
+        JacksonConfig config = new JacksonConfig();
+        ObjectMapper mapper = config.objectMapper();
+        assertNotNull(mapper);
+        assertFalse(mapper.getRegisteredModuleIds().contains(JavaTimeModule.class.getName()));
+    }
+
+    // RestTemplate bean is created successfully
+    @Test
+    public void rest_template_bean_creation() {
+        AppConfig appConfig = new AppConfig();
+        RestTemplate restTemplate = appConfig.restTemplate();
+        assertNotNull(restTemplate);
+        assertTrue(restTemplate.getMessageConverters().stream()
+                .anyMatch(converter -> converter instanceof MappingJackson2HttpMessageConverter));
+    }
 }
